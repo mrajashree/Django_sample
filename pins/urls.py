d2a8304718db48900631166c128b1895719a3249
Rajashree Mandaogane <rajashree.28m@gmail.com>
2016-03-14 16:41:55 -0400

    Urls for views

--- pins/urls.py
+++ pins/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.profile, name='profile'),
+	url(r'^$', views.PinsDisplay, name='PinsDisplay'),
+	url(r'^(?P<id>[0-9]+)/$', views.PinDescription, name='PinDescription'),
 ]
