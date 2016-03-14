26a480a8d0534d399ac4be0bb065bb6ea54e8715
Rajashree Mandaogane <rajashree.28m@gmail.com>
2016-03-14 16:41:44 -0400

    Create tables

--- pins/models.py
+++ pins/models.py
@@ -3,3 +3,26 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+PRIVACY_CHOICES = (
+	('PR', 'Private'),
+	('PB', 'Public'),
+)
+
+class Pins(models.Model):
+	name		= models.CharField(max_length=200)
+	slug		= models.SlugField(unique=True)
+	category	= models.ForeignKey('Category')
+	privacy		= models.CharField(max_length=2,choices=PRIVACY_CHOICES)
+	description	= models.TextField(blank=True)
+
+	def __unicode__(self):
+		return self.name
+
+class Category(models.Model):
+	name		= models.CharField(max_length=200)
+	slug 		= models.SlugField(unique=True)
+	description	= models.TextField(blank=True)
+
+	def __unicode__(self):
+		return self.name
